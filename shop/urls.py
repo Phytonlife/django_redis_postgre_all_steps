130b7c67b6de9ead6e3fde03b43129286942021b
lvlit <player14017@gmail.com>
2025-07-18 17:36:37 +0500

    сделали динамичесский url

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from shop import views
 
 urlpatterns = [
-    path("",views.index)
+    path("",views.index),
+    path("category/<slug:cat_slug>",views.categories_by_slug)
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Страница приложения shop")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p >slug:{ cat_slug }</p>")
